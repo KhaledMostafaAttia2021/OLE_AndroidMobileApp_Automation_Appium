d536c5c84f89b673389b9b3e5bf2826da9aeadf1
Khaled2021 <Khaled2021@Khaled>
2019-07-21 15:56:41 +0200

    KHA: OLE

--- OLE_AndroidMobileApp_Automation_Appium/src/test/java/tests/demoTesting_Mobile.java
+++ OLE_AndroidMobileApp_Automation_Appium/src/test/java/tests/demoTesting_Mobile.java
@@ -2,11 +2,12 @@ package tests;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.appium.java_client.android.AndroidDriver;
@@ -22,40 +23,67 @@ public class demoTesting_Mobile {
 				"D:\\Orange_Task_July_2019\\AndroidApp\\Pricena Shopping Comparison_v4.2.1_apkpure.com.apk");
 		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
 
-		driver.findElement(By.name("zone_id"));
-		WebElement objWeb1 = driver.findElement(By.name("zone_id"));
-		Select countrySelector = new Select(objWeb1);
-		countrySelector.deselectByVisibleText("Egypt");
+		WebElement countrySelector = driver.findElement(By.id("Egypt"));
+		countrySelector.click();
 
 		WebElement searchTxt = driver.findElement(By.id("search_bar"));
+		searchTxt.isEnabled();
 		searchTxt.sendKeys("fan 26");
 
-		WebElement searchBtn = driver.findElement(By.id("search_button"));
-		searchBtn.click();
+		driver.findElement(By.id("fan 26")).sendKeys(Keys.ENTER);
 
 		WebElement filterBtn = driver.findElement(By.id("filter_button"));
 		filterBtn.click();
 
-		driver.findElement(By.name("filter_id"));
-		WebElement objWeb2 = driver.findElement(By.name("filter_id"));
-		Select filterOption = new Select(objWeb2);
-		filterOption.deselectByVisibleText("JAC");
+		WebElement brand_Filt = driver.findElement(By.id("brand_filter"));
+		brand_Filt.click();
 
-		WebElement submitFilterBtn = driver.findElement(By.id("submit_filter_button"));
-		submitFilterBtn.click();
+		// Find the check box element by its name.
+		List<WebElement> list = driver.findElements(By.name("checkbox"));
 
-		WebElement displayedPrice = driver.findElement(By.name("displayedHuPrice"));
-		WebElement displayedName = driver.findElement(By.name("displayedHuName"));
-		WebElement displayedVendor = driver.findElement(By.name("displayedHuVendor"));
+		// Get the number of check boxes available.
+		int count = list.size();
 
-		WebElement huaweiPhone1 = driver.findElement(By.linkText("HuawaiPhoneP8Lite"));
-		huaweiPhone1.click();
+		// Now, iterate the loop from first check box to last check box.
+		for (int i = 0; i < count; i++) {
+
+			// Store the check box name to the string variable, using 'Value'
+			// attribute
+			String sValue = ((WebElement) list.get(i)).getAttribute("Jac");
+
+			// Select the check box if its value is the same that you want.
+			if (sValue.equalsIgnoreCase("checkbox")) {
+
+				((WebElement) list.get(i)).click();
+
+				// This statement will get you out of the for loop.
+				break;
+			}
+
+		}
+
+		WebElement applyBtn_F_Time = driver.findElement(By.id("applyBtnF_id"));
+		applyBtn_F_Time.click();
+
+		Thread.sleep(3000);
+
+		WebElement applyBtn_Sec_Time = driver.findElement(By.id("applyBtnSec_id"));
+		applyBtn_Sec_Time.click();
 
 		Thread.sleep(1000);
 
-		WebElement price = driver.findElement(By.name("HuPrice"));
-		WebElement name = driver.findElement(By.name("HuName"));
-		WebElement vendor = driver.findElement(By.name("HuVendor"));
+		WebElement displayedPrice = driver.findElement(By.name("displayedFan26Price"));
+		WebElement displayedName = driver.findElement(By.name("displayedFan26Name"));
+		WebElement displayedVendor = driver.findElement(By.name("displayedFan26Vendor"));
+
+		WebElement Fan = driver.findElement(By.linkText("Fan26"));
+		Fan.click();
+
+		Thread.sleep(1000);
+
+		WebElement price = driver.findElement(By.name("Fan26Price"));
+		WebElement name = driver.findElement(By.name("Fan26Name"));
+		WebElement vendor = driver.findElement(By.name("Fan26Vendor"));
 
 		Assert.assertEquals(price.getText(), displayedPrice.getText());
 		Assert.assertEquals(name.getText(), displayedName.getText());
